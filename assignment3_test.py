213bd17a8061d672005106ff4e8f0d36178f70bf
Talha Rizwan <79090811+TalhaRizwan093@users.noreply.github.com>
2023-12-16 20:34:43 +0500

    test added

---
+++ assignment3_test.py
@@ -0,0 +1,40 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+
+options = Options()
+options.add_argument('--headless')
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
+browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+
+browser.get("http://44.220.249.251:3001/")
+
+browser.implicitly_wait(0.5)
+
+title_web = browser.find_element_by_class_name("navbar__logo")
+
+
+# first_name = browser.find_element(By.ID, "input-firstname")
+# last_name = browser.find_element(By.ID, "input-lastname")
+# telephone = browser.find_element(By.ID, "input-telephone")
+# email = browser.find_element(By.ID, "input-email")
+# password = browser.find_element(By.ID, "input-password")
+# password_confirm = browser.find_element(By.ID, "input-confirm")
+# terms = browser.find_element(By.XPATH, value="//label[@for='input-agree']")
+# continue_button = browser.find_element(By.XPATH, value="//input[@value='Continue']")
+
+# first_name.send_keys("FFirstName")
+# last_name.send_keys("LLastName")
+# email.send_keys("sample-email1@data.com")
+# telephone.send_keys("+351999888888")
+# password.send_keys("12345678")
+# password_confirm.send_keys("12345678")
+# terms.click()
+# continue_button.click()
+
+print(title_web)
+assert true
