851b4d23c7f899bbe093c11d9b33836bc7e4b5b7
Talha Rizwan <79090811+TalhaRizwan093@users.noreply.github.com>
2023-12-16 19:59:56 +0500

    db value changed

--- e-comerce-backend/backend/config/db.js
+++ e-comerce-backend/backend/config/db.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose')
 const connectDB = async () => {
   try {
     await mongoose.connect(
-      process.env.MONGO_URI || 'mongodb://database:27017/ecom',
+      process.env.MONGO_URI,
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
