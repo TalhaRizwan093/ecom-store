259e084ccf1ecd861a58c7f9a49eb3e9206e714b
Talha Rizwan <79090811+TalhaRizwan093@users.noreply.github.com>
2023-12-17 12:53:07 +0500

    .

--- assignment3_test.py
+++ assignment3_test.py
@@ -96,6 +96,8 @@ print(nav_links_logout_li)
 nav_links_logout = nav_links_logout_li[2]
 print(nav_links_logout)
 nav_links_logout_text = nav_links_logout.find_element(By.TAG_NAME, "p")
+logout_text = nav_links_logout_text.get_attribute("innerHTML")
+assert logout_text == "Logout"
 print(nav_links_logout_text.get_attribute("innerHTML"))
 print("logout", nav_links_logout_text)
 
