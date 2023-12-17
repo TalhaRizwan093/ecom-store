5624d3a590d185f3fcee73fdbe2f61b6628f89b1
Talha Rizwan <79090811+TalhaRizwan093@users.noreply.github.com>
2023-12-17 12:47:19 +0500

    .

--- assignment3_test.py
+++ assignment3_test.py
@@ -96,5 +96,5 @@ print(nav_links_logout_li)
 nav_links_logout = nav_links_logout_li[2]
 print(nav_links_logout)
 nav_links_logout_text = nav_links_logout.find_elements(By.TAG_NAME, "p")
-print(nav_links_logout_text[0].text)
+print(nav_links_logout_text.text)
 
